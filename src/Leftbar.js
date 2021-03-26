7c8cde076fbb342e26eca491f7c7cc27110b763b
tortinaja <79805888+tortinaja@users.noreply.github.com>
2021-03-26 15:39:42 +0700

    Leftbar again

--- src/Leftbar.js
+++ src/Leftbar.js
@@ -4,6 +4,8 @@ function Leftbar () {
     return (
         <section className="Leftbar">
             <div className="inLeftbar1">New Feed</div>
+            <div className="inLeftbar2">ข้อมูลส่วนตัว</div>
+            <div className="inLeftbar3">Contract</div>
         </section>
     )
 };
